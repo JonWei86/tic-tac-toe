3c98900d82f11220b15958d50246559c297bc91c
jon <jkwei.86@gmail.com>
2024-11-05 13:09:25 -0500

    refactor: refactors script.js and removes console logs

--- script.js
+++ script.js
@@ -49,8 +49,22 @@
     }
     
     function checkWinner(){
+        const winningSpaces = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],]
+
         let allWinningSets = [];
-    
+        
+        for (let set of winningSpaces){
+            createWinningSet(set);
+        }
+        
         function createWinningSet(spaces){
             let winningSet = []
             for (let space of spaces){
@@ -60,36 +74,12 @@
             allWinningSets.push(winningSet);
             return winningSet;
         }
-    
-        const row1 = createWinningSet([0,1,2]);
-        const row2 = createWinningSet([3,4,5]);
-        const row3 = createWinningSet([6,7,8]);
-    
-        const col1 = createWinningSet([0,3,6]);
-        const col2 = createWinningSet([1,4,7]);
-        const col3 = createWinningSet([2,5,8]);
-    
-        const diag1 = createWinningSet([0,4,8]);
-        const diag2 = createWinningSet([2,4,6]);
-    
-        console.log("row 1", row1);
-        console.log("row 2", row2);
-        console.log("row 3", row3);
-        console.log("col 1", col1);
-        console.log("col 2", col2);
-        console.log("col 3", col3);
-        console.log("diag 1", diag1);
-        console.log("diag 2", diag2);
 
         for(let set of allWinningSets){
-            console.log('running', set)
-            console.log('index of', allWinningSets.indexOf(set))
                 if (set.join() === "X,X,X" || set.join() === "O,O,O"){
                     createModal(`<p>Player ${gameBoard.playersTurn} won!</p>`);
                     if(set.join() === "O,O,O") {++player1.score; updateScores(player1);}
                     else {++player2.score; updateScores(player2);}
-                    console.log('player1 score:', player1.score)
-                    console.log('player2 score:', player2.score)
                     return set
                 }else{
                     if(allWinningSets.flat().filter((element) => element === null).length === 0 && allWinningSets.indexOf(set) === allWinningSets.length - 1){
@@ -105,33 +95,32 @@
     }
 
     function createModal(msg){
-        //create backdrop
+        //CREATE backdrop
         const modalContainer = document.createElement("div");
         modalContainer.classList.add('modal-container');
-        //create modal window
+        //CREATE modal window
         const modal = document.createElement("div");
         modal.classList.add("modal");
         modal.innerHTML=`${msg}`;
-        //append modal window
+        //APPEND modal window
         modalContainer.appendChild(modal);
         const btnContainer = document.createElement("div");
         btnContainer.classList.add("btn-container");
         modal.appendChild(btnContainer);
-        //create buttons
-        //create playAgain-btn
+        //CREATE playAgain-btn
         const playAgainBtn = document.createElement("button");
         playAgainBtn.classList.add("play-again-btn");
         playAgainBtn.addEventListener('click', ()=>{playAgain(modalContainer)});
         playAgainBtn.innerText= 'Play Another Round';
-        //create resetBtn
+        //CREATE resetBtn
         const resetBtn = document.createElement("button");
         resetBtn.classList.add("reset-btn");
         resetBtn.innerText= 'Reset Game';
         resetBtn.addEventListener('click', ()=>{reset(modalContainer)});
-        //appened buttons
+        //APPEND buttons
         btnContainer.appendChild(playAgainBtn);
         btnContainer.appendChild(resetBtn);
-        //appened modal container and window
+        //APPEND modal container and window
         document.querySelector(".board-container").appendChild(modalContainer);
     }
 
